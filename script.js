7643f1a97464d72e34caacfaf9e4206fac863802
pRostok <rostmart29@gmail.com>
2022-08-11 23:18:25 -0700

    add script

---
+++ script.js
@@ -0,0 +1,65 @@
+const message = document.getElementById('message')
+const shift = document.getElementById('shift')
+const output = document.getElementById('output')
+
+
+
+message.addEventListener("input", () => {
+    let itsValue = this.value;
+    output.textContent = rot13({
+        msg: itsValue,
+        shift: shift.value
+    });
+});
+
+shift.addEventListener("keyup", function () {
+    let itsValue = this.value;
+    output.textContent = rot13({
+        msg: message.value,
+        shift: itsValue
+    })
+});
+
+output.textContent = rot13({
+    msg: message.value,
+    shift: shift.value
+});
+
+
+
+
+
+
+function rot13(str) {
+
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toUpperCase(),
+        shiftAmount,
+        shiftedAlphabet = "",
+        output = "",
+        upperString,
+
+        otherCharacters = '-=~\"\'#$%&*^:<>?/!{(|)}.1234567890\, '
+
+    if (typeof str === "object") {
+        shiftAmount = str.shift;
+        upperString = str.msg.toUpperCase();
+    } else {
+        return;
+    }
+
+    if (typeof upperString === "string" || typeof (upperString + "") === "string") {
+        shiftedAlphabet = alphabet.slice(shiftAmount);
+        shiftedAlphabet += alphabet.slice(0, shiftAmount);
+        shiftedAlphabet += otherCharacters;
+        alphabet += otherCharacters;
+
+        for (let i = 0; i < upperString.length; i++) {
+            let numberd = alphabet.indexOf(upperString[i])
+            output += shiftedAlphabet[numberd];
+        }
+    } else {
+        return;
+    };
+
+    return output;
+};
