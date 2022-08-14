913fc0be4af29f08c6f6235bde48454c81536269
Vadym Hulchenko <vadymhulchenko@gmail.com>
2022-08-14 11:53:03 -0600

    clean up the function a bit, fix toUpperCase console error

--- script.js
+++ script.js
@@ -1,74 +1,62 @@
-const message = document.getElementById('message')
-const shift = document.getElementById('shift')
-const output = document.getElementById('output')
+const message = document.getElementById('message');
+const shift = document.getElementById('shift');
+const output = document.getElementById('output');
 
+message.addEventListener('keyup', function () {
+  const nums = /[0-9]/g;
+  this.value = this.value.replace(nums, '');
 
+  output.textContent = rot13({
+    msg: this.value,
+    shift: shift.value,
+  });
+});
 
-message.addEventListener('keyup', function ()  {
-    let dont = /[0-9]/g;
-    let itsValue = this.value
-    this.value = this.value.replace(dont, '')
-
-    message.addEventListener("input", () => {
-        let itsValue = this.value;
-        
-        output.textContent = rot13({
-            
-            msg: this.Value,
-            shift: shift.value
-        });
-    });
-
-})
-
-shift.addEventListener("keyup", function () {
-    let itsValue = this.value;
-    
-    output.textContent = rot13({
-        msg: message.value,
-        shift: itsValue
-    })
+shift.addEventListener('keyup', function () {
+  output.textContent = rot13({
+    msg: message.value,
+    shift: this.value,
+  });
 });
 
 output.textContent = rot13({
-    
-    msg: message.value,
-    shift: shift.value
+  msg: message.value,
+  shift: shift.value,
 });
 
-
 function rot13(str) {
+  console.log(`TRIGGER`);
+  let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.toUpperCase(),
+    shiftAmount,
+    shiftedAlphabet = '',
+    output = '',
+    upperString,
+    otherCharacters = '-=~"\'#$%&*^:<>?/!{(|)}.1234567890,';
 
-    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toUpperCase(),
-        shiftAmount,
-        shiftedAlphabet = "",
-        output = "",
-        upperString,
+  if (typeof str === 'object') {
+    console.log(str);
+    shiftAmount = str.shift;
+    upperString = str.msg.toUpperCase();
+  } else {
+    return;
+  }
 
-        otherCharacters = '-=~\"\'#$%&*^:<>?/!{(|)}.1234567890\,'
-    
-        
-    if (typeof str === "object") {
-        shiftAmount = str.shift;
-        upperString = str.msg.toUpperCase();
-    } else {
-        return;
+  if (
+    typeof upperString === 'string' ||
+    typeof (upperString + '') === 'string'
+  ) {
+    shiftedAlphabet = alphabet.slice(shiftAmount);
+    shiftedAlphabet += alphabet.slice(0, shiftAmount);
+    shiftedAlphabet += otherCharacters;
+    alphabet += otherCharacters;
+
+    for (let i = 0; i < upperString.length; i++) {
+      let numberd = alphabet.indexOf(upperString[i]);
+      output += shiftedAlphabet[numberd];
     }
+  } else {
+    return;
+  }
 
-
-    if (typeof upperString === "string" || typeof (upperString + "") === "string") {
-        shiftedAlphabet = alphabet.slice(shiftAmount);
-        shiftedAlphabet += alphabet.slice(0, shiftAmount);
-        shiftedAlphabet += otherCharacters;
-        alphabet += otherCharacters;
-
-        for (let i = 0; i < upperString.length; i++) {
-            let numberd = alphabet.indexOf(upperString[i])
-            output += shiftedAlphabet[numberd];
-        }
-    } else {
-        return;
-    };
-
-    return output;
-};
+  return output;
+}
